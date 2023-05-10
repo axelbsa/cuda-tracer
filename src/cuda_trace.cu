6dc584a1d20da8e3516f62308b389ec38a6f994d
Axel Sanner <axel@xeneta.com>
2023-05-11 01:07:49 +0200

    Create Material interface and add Lambertian and Metal
    Also refactor some code away into the ever growing raytracer.h

--- src/cuda_trace.cu
+++ src/cuda_trace.cu
@@ -8,42 +8,21 @@
 
 #include "raytracer.h"
 
-typedef unsigned char uchar;
-
-//const double infinity = std::numeric_limits<double>::infinity();
-const double pi = 3.1415926535897932385;
-
-#define RANDVEC3 make_float3(curand_uniform(local_rand_state),curand_uniform(local_rand_state),curand_uniform(local_rand_state))
-__device__ float3 random_in_unit_sphere(curandState *local_rand_state) {
-    float3 p;
-    do {
-        p = 2.0f*RANDVEC3 - make_float3(1,1,1);
-    } while (length_squared(p) >= 1.0f);
-    return p;
-}
-
-__device__ __inline__ float degrees_to_radians(float degrees) {
-    return degrees * pi / 180.0;
-}
-
-__device__ __inline__ uchar4 to_uchar4(float4 vec) {
-    return make_uchar4((uchar)vec.x, (uchar)vec.y, (uchar)vec.z, (uchar)vec.w);
-}
-
-
 __device__ float3 color(const Ray& r, Hittable **world, curandState *local_rand_state) {
     Ray cur_ray = r;
-    float cur_attenuation = 1.0f;
+    float3 cur_attenuation = make_float3(1.0f, 1.0f, 1.0f);
     for (int i = 0; i < 50; ++i) {
         hit_record rec;
+        float3 attenuation;
         if ((*world)->hit(cur_ray, 0.001f, FLT_MAX, rec)) {
-            float3 target = rec.p + rec.normal + random_in_unit_sphere(local_rand_state);
-            cur_attenuation *= 0.5f;
-            cur_ray = Ray(rec.p, target - rec.p);
+            if (rec.mat_prt->scatter(r, rec, attenuation, cur_ray, local_rand_state))
+            {
+                cur_attenuation *= attenuation;
+            }
         } else {
             float3 unit_direction = unit_vector(cur_ray.direction());
             float t = 0.5f * (unit_direction.y + 1.0f);
-            float3 c = (1.0f - t) * make_float3(1.0, 1.0, 1.0) + t * make_float3(0.5, 0.7, 1.0);
+            float3 c = (1.0f - t) * make_float3(1.0f, 1.0f, 1.0f) + t * make_float3(0.5f, 0.7f, 1.0f);
             return cur_attenuation * c;
         }
     }
@@ -64,7 +43,7 @@ __global__ void d_render(
 
     uint i = y * imageW + x;  // Array transformation to 1D?
     curandState local_rand_state = d_rand_state[i];
-#define ns 50
+#define ns 1
     for(int s=0; s < ns; s++) {
         float u = float(x + curand_uniform(&local_rand_state)) / float(imageW);
         float v = float(y + curand_uniform(&local_rand_state)) / float(imageH);
@@ -93,10 +72,19 @@ __global__ void render_init(int max_x, int max_y, curandState *rand_state) {
 
 __global__ void create_world(dim3 windowSize, Hittable **d_list, Hittable **d_world, Camera **d_camera)
 {
+    // Make sure we only do this once
     if (threadIdx.x == 0 && blockIdx.x == 0) {
-        *(d_list) = new Sphere(make_float3(0.0f, 0.0f, -1.0f), 0.5f);
-        *(d_list + 1) = new Sphere(make_float3(0.0f, -100.5f,-1.0f), 100.0f);
-        *d_world = new Hittable_list(d_list, 2);
+        Material *mat_small_lamb = new Lambertian(make_float3(0.8f, 0.3f, 0.3f));
+        Material *mat_big_lamb = new Lambertian(make_float3(0.8f, 0.8f, 0.0f));
+        Material *mat_small_met_1 = new Metal(make_float3(0.8f, 0.6f, 0.2f), 0.3f);
+        Material *mat_small_met_2 = new Metal(make_float3(0.8f, 0.8f, 0.8f), 1.0f);
+
+        *(d_list + 0) = new Sphere(make_float3(0.0f, 0.0f, -1.0f), 0.5f, mat_small_lamb);
+        *(d_list + 1) = new Sphere(make_float3(0.0f, -100.5f,-1.0f), 100.0f, mat_big_lamb);
+        *(d_list + 2) = new Sphere(make_float3(1.0f, 0.0f,-1.0f), 0.5f, mat_small_met_1);
+        *(d_list + 3) = new Sphere(make_float3(-1.0f, 0.0f,-1.0f), 0.5f, mat_small_met_2);
+
+        *d_world = new Hittable_list(d_list, 4);
         *d_camera = new Camera(windowSize);
     }
 }
@@ -140,7 +128,6 @@ extern "C" void trace(
         Hittable **d_world,
         Camera **d_camera)
 {
-    //fprintf(stderr, "Calling draw\n");
     d_render<<<blocks, threads>>>(
             d_output, imageW, imageH, d_world, d_camera, d_rand_state);
 
--- src/raytracer.h
+++ src/raytracer.h
@@ -1,21 +1,50 @@
 #ifndef RAY_H
 #define RAY_H
 
+#include <curand_kernel.h>
+#include "cuda_helpers/helper_cuda.h"
 #include "cuda_helpers/helper_math.h"
 
+class Material;  // Define some class material
+typedef unsigned char uchar;
+const double pi = 3.1415926535897932385;
+
+
+//__device__ __inline__ float3 reflect()
+
+__device__ __inline__ float degrees_to_radians(float degrees) {
+    return degrees * pi / 180.0;
+}
+
+__device__ __inline__ uchar4 to_uchar4(float4 vec) {
+    return make_uchar4((uchar)vec.x, (uchar)vec.y, (uchar)vec.z, (uchar)vec.w);
+}
+
 // Most math functions will be in the above include.
-inline __host__ __device__ float3 unit_vector(float3 v)
+__host__ __device__ inline float3 unit_vector(float3 v)
 {
     return v / length(v);
 }
 
-inline __host__ __device__ float length_squared(float3 v)
+__host__ __device__ inline float length_squared(float3 v)
 {
     return v.x * v.x + v.y * v.y + v.z * v.z;
 }
 
-class Ray
-{
+#define RANDVEC3 make_float3(     \
+    curand_uniform(local_rand_state), \
+    curand_uniform(local_rand_state), \
+    curand_uniform(local_rand_state)  \
+)
+__device__ static inline float3 random_in_unit_sphere(curandState *local_rand_state) {
+    float3 p;
+    do {
+        p = 2.0f*RANDVEC3 - make_float3(1,1,1);
+    } while (length_squared(p) >= 1.0f);
+    return p;
+}
+
+class Ray {
 public:
     __device__ Ray() {}
     __device__ Ray(const float3& a, const float3& b) { A = a; B = b; }
@@ -32,6 +61,7 @@ struct hit_record {
     float3 p;
     float3 normal;
     float t;
+    Material *mat_prt;
 
     bool front_face;
 
@@ -42,8 +72,7 @@ struct hit_record {
 };
 
 
-class Camera
-{
+class Camera {
 public:
     __host__ __device__ Camera(dim3 windowSize)
     {
@@ -85,18 +114,15 @@ public:
 
 
 class Hittable {
-    public:
-        __device__ virtual bool hit(const Ray& r, double t_min, double t_max, hit_record& rec) const = 0;
+public:
+    __device__ virtual bool hit(const Ray& r, double t_min, double t_max, hit_record& rec) const = 0;
 };
 
 
 class Sphere : public Hittable {
 public:
     __device__ Sphere() {}
-    __device__ Sphere(float3 cen, double r) : center(cen), radius(r) {};
-
-    //__host__ __device__ bool hit(const Ray& r, double t_min, double t_max, hit_record& rec) const override;
-
+    __device__ Sphere(float3 cen, double r, Material *m) : center(cen), radius(r), mat_ptr(m) {};
 
     __device__ bool hit(const Ray& r, double t_min, double t_max, hit_record& rec) const
     {
@@ -127,12 +153,14 @@ public:
         rec.normal = (rec.p - center) / radius;
         float3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat_prt = mat_ptr;
 
         return true;
     }
 
     float3 center;
     float radius;
+    Material *mat_ptr;
 };
 
 
@@ -152,7 +180,6 @@ public:
         auto closest_so_far = t_max;
 
         for (int i = 0; i < list_size; ++i) {
-            //for (const auto& object : list) {
             if (list[i]->hit(r, t_min, closest_so_far, temp_rec)) {
                 hit_anything = true;
                 closest_so_far = temp_rec.t;
@@ -166,8 +193,62 @@ public:
 private:
     int list_size;
     Hittable **list;
-    //std::vector<make_shared<>(Hittable)> objects;
-    //std::vector<shared_ptr<Hittable>> objects;
+};
+
+class Material {
+public:
+    __device__ virtual bool scatter(
+            const Ray& r_in,
+            const hit_record& rec,
+            float3& attenuation,
+            Ray& scattered,
+            curandState *local_rand_state
+    ) const = 0;
+};
+
+class Lambertian : public Material {
+public:
+    __device__ Lambertian ( const float3& a ) : albedo(a) {}
+
+    __device__ bool scatter(const Ray& r_in, const hit_record& rec,
+                            float3& attenuation, Ray& scattered, curandState *local_rand_state) const
+    {
+        float3 target = rec.p + rec.normal + random_in_unit_sphere(local_rand_state); // A new vector in a random direction from where it hit
+        scattered = Ray(rec.p, target - rec.p); // A new ray with vector pointing at target - rec.p (eye)
+        attenuation = albedo; // Attenuation == how much light do we have left 0.0 - 1.0
+                              // In this case it's just the color of the object
+        return true;
+    }
+
+    float3 albedo;
+};
+
+
+class Metal : public Material {
+public:
+    __device__ Metal (const float3& a, float f) {
+        albedo = a;
+        (f < 1.0f) ? fuzzy = f : fuzzy = 1;
+
+    }
+    __device__ bool scatter(const Ray& r_in, const hit_record& rec,
+                            float3& attenuation, Ray& scattered, curandState *local_rand_state) const
+    {
+        // Metals are like perfect mirrors if they are smooth enough
+        // So we take the ray going into the scene, get the normal of the object we are hitting
+        // And call reflect to give us the reflected new vector.
+        float3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+        scattered = Ray(rec.p, reflected + fuzzy * random_in_unit_sphere(local_rand_state)); // Create a new ray with the new vector
+        attenuation = albedo; // Same as above, light radiation is just the color of the object
+
+        // Check if the angle between the incomming ray's normal (rec.normal) and the new reflected ray (scattered)
+        // Is greater than 0.0f. Try to imagine where the ray would be 0
+        // https://www.haroldserrano.com/blog/vectors-in-computer-graphics
+        return (dot(scattered.direction(), rec.normal)) > 0;
+    }
+
+    float3 albedo;
+    float fuzzy;
 };
 
 #endif
